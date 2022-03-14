43c0584cb9c2c294d5d69402e66797da22c42567
Rodrigo Silva <rodrigooosc@outlook.com>
2022-03-14 18:35:37 -0300

    ngIf e  Propriedade Binding Parte 1/2

--- Front-end/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front-end/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,18 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  // Possui elementos vazios - array
+  public eventos: any = [];
+ // ---- PROPERTY BINDING --- //
+ // Definir tamanho de imagem
+ widthImg: number = 150;
+ // Definir margem da imagem
+ marginImg: number = 2;
+ // Mostrar imagem
+ displayImg: boolean = true;
+
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+    
+  }
+
+  exibirImagem() {
+    this.displayImg = !this.displayImg;
   }
 
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
     response => this.eventos = response,
     error => console.log(error)
-    );    
+    );
   }
 }
