59562ac307c8278f924ecea1c08f27e85dbc09e7
Rodrigo Silva <rodrigooosc@outlook.com>
2022-03-15 13:55:32 -0300

    Filtrando Grid

--- Front-end/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front-end/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,6 +10,8 @@ export class EventosComponent implements OnInit {
 
   // Possui elementos vazios - array
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+
  // ---- PROPERTY BINDING --- //
  // Definir tamanho de imagem
  widthImg: number = 150;
@@ -18,8 +20,24 @@ export class EventosComponent implements OnInit {
  // Mostrar imagem
  displayImg: boolean = true;
 
- filtroLista: string = ' ';
+ private _filtroLista: string = '';
 
+ public get filtroLista(){
+   return this._filtroLista
+ }
+
+ public set filtroLista(value: string){
+  this._filtroLista = value;
+  this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+}
+
+filtrarEventos(filtrarPor: string) : any {
+  filtrarPor = filtrarPor.toLocaleLowerCase();
+  return this.eventos.filter((evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -  1 ||
+  evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -  1  ||
+  evento.dataEvento.toLocaleLowerCase().indexOf(filtrarPor) !== -  1 
+  );
+}
 
   constructor(private http: HttpClient) { }
 
@@ -34,7 +52,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-    response => this.eventos = response,
+    response => {
+      this.eventos = response;
+      this.eventosFiltrados = this.eventos
+    },
     error => console.log(error)
     );
   }
