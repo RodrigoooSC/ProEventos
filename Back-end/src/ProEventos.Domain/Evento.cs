dfad4a11707b496941e1f0f9142bc4e17220d754
Rodrigo Silva <rodrigooosc@outlook.com>
2022-03-15 16:21:32 -0300

    Entidades domínio

---
+++ Back-end/src/ProEventos.Domain/Evento.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace ProEventos.Domain
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+
+        public string Local { get; set; }
+
+        public DateTime? DataEvento { get; set; } // Iniciar podendo ser nulo
+
+        public string Tema { get; set; }
+
+        public int QntPessoas { get; set; }
+        
+        public string ImagemURL { get; set; }
+
+        public string Telefone { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<Lote> Lote { get; set; }
+
+        public IEnumerable<RedeSocial> RedeSociais { get; set; }
+        public IEnumerable<PalestranteEvento> PalestrantesEventos { get; set; }
+    }        
+}
