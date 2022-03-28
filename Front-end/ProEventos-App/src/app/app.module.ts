f55645ee05e95d928637c876cd1425fe08ab9570
Rodrigo Silva <rodrigooosc@outlook.com>
2022-03-28 16:14:34 -0300

    Tooltip e Dropdown

--- Front-end/ProEventos-App/src/app/app.module.ts
+++ Front-end/ProEventos-App/src/app/app.module.ts
@@ -1,18 +1,26 @@
+// ANGULAR
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// COMPONENTS
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
 import { NavComponent } from './nav/nav.component';
 
+// NGX-BOOTSTRAP
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
+// SERVICES
 import { EventoService } from './services/evento.service';
+
+// PIPES
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 
 
@@ -30,6 +38,8 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
     FormsModule
   ],
   providers: [EventoService],
